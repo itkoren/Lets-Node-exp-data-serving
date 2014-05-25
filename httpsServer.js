7861d5aa2180cff9e1c27a18086cbb0f2e47dfca
Itai Koren <itkoren@gmail.com>
2014-05-25 12:02:40 +0300

    Add HTTPS Example
    Signed-off-by: Itai Koren <itkoren@gmail.com>

---
+++ httpsServer.js
@@ -0,0 +1,17 @@
+// Include The 'http' Module
+var https = require("https");
+
+var fs = require("fs");
+
+var options = {
+    key: fs.readFileSync("./key.pem"),
+    cert: fs.readFileSync("./key-cert.pem")
+};
+
+// Create the HTTP Server
+var server = https.createServer(options, function(req, res) {
+    // Handle HTTPS Request
+    res.end("Hello World");
+}).listen(process.env.PORT || 8443, process.env.HOST || "0.0.0.0", function() {
+    console.log("HTTP Server Started. Listening on " + server.address().address + " : Port " + server.address().port);
+});
